58c23bf9e6b98319fae9d8852e331004a49926eb
Jialu Chen <jchen553@wisc.edu>
2024-05-23 10:31:06 +0800

    Update run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-
-
 python train.py \
     --dataset=cora  \
     --SEED=0 \
@@ -33,65 +31,4 @@ python train.py \
     --metric='jaccard'
 
 
-python train.py \
-    --dataset=pubmed  \
-    --SEED=0 \
-    --K=100 \
-    --lr_p=0.001 \
-    --wd_p=0.00001 \
-    --num_epochs=2000 \
-    --num_hidden=256 \
-    --tau1=1.0 \
-    --l1=100.0 \
-    --optimizer='adam' \
-    --metric='jaccard' \
-    --drop_edge_rate=0.9 \
-    
-python train.py \
-    --dataset=DBLP  \
-    --SEED=0 \
-    --K=10 \
-    --lr_p=0.001 \
-    --lr_m=0.01 \
-    --wd_p=0.00001 \
-    --wd_m=0.0 \
-    --num_epochs=1500 \
-    --num_hidden=256 \
-    --tau1=1.0 \
-    --l1=100.0 \
-    --optimizer='adam' \
-    --metric='jaccard' \
-    --drop_edge_rate=0.4 \
-    --drop_kg_edge_rate=0.5
-
-
-
-python train.py \
-    --dataset=Photo  \
-    --SEED=0 \
-    --K=1 \
-    --lr_p=0.001 \
-    --wd_p=0.0001 \
-    --num_epochs=1000 \
-    --num_hidden=256 \
-    --tau1=0.8 \
-    --l1=100 \
-    --optimizer='adam' \
-    --drop_edge_rate=0.9 \
-    --drop_kg_edge_rate=0.5
-    
-python train.py \
-    --dataset=CS  \
-    --SEED=0 \
-    --K=100 \
-    --lr_p=0.001 \
-    --wd_p=0.0001 \
-    --num_epochs=1000 \
-    --num_hidden=256 \
-    --tau1=1.0 \
-    --l1=100 \
-    --optimizer='adam' \
-    --drop_edge_rate=0.9 \
-    --drop_kg_edge_rate=0.5
-    
     
