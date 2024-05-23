48ef699de161e0557cf2bac7f54276631e9c5017
Jialu Chen <jchen553@wisc.edu>
2024-05-23 10:49:28 +0800

    Update run.sh

--- code/run.sh
+++ code/run.sh
@@ -31,4 +31,20 @@ python train.py \
     --metric='jaccard'
 
 
+python train.py \
+    --dataset=cora  \
+    --SEED=0 \
+    --K=100 \
+    --lr_p=0.001 \
+    --wd_p=0.0001 \
+    --num_epochs=500 \
+    --num_hidden=256 \
+    --tau1=0.2 \
+    --l1=1.0 \
+    --metric='jaccard' \
+    --optimizer='adam' \
+    --drop_edge_rate=0.1 \
+    --drop_kg_edge_rate=0.2 \
+    --task='link'
+    
     
